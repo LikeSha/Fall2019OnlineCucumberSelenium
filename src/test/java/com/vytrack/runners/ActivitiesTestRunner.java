0c88e3762f453039fcdfa10c4558d83ba56dcf92
Like Sha <tradersalike88@yahoo.com>
2020-04-19 10:50:06 -0700

    adjust codes

--- src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
+++ src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = "com/vytrack/step_definitions",
         features = "src/test/resources/features/activities",
-        dryRun = true,
+        dryRun = false,
         strict = false,
         tags = "",
         plugin = {
