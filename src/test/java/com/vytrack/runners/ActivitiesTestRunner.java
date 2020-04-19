fdca3e542dd74d5a98c9e45463dbc9dfc19e98f2
Like Sha <tradersalike88@yahoo.com>
2020-04-19 12:03:41 -0700

    adjust codes

--- src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
+++ src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         tags = "",
         plugin = {
                 "html:target/default-report",
-                "json:target/cucumber.json"
+                "json:target/cucumber1.json"
         }
 
 )
