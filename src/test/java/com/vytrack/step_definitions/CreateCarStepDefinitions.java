648c477061178920c4b159f112f29e0e0df2640a
Like Sha <tradersalike88@yahoo.com>
2020-04-19 13:46:09 -0700

    updated vehiclepage class

--- src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -1,4 +1,21 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.fleet.VehiclesPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.tr.Ve;
+
 public class CreateCarStepDefinitions {
+     VehiclesPage vehiclesPage = new VehiclesPage();
+
+     @Then("user click on create a car button")
+    public void user_click_on_create_a_car_button() {
+         System.out.println("User click on create a car button");
+         VehiclesPage.clickToCreate
+    }
+
+    @Then("user creates a car with following info:")
+    public void user_creates_a_car_with_following_info(List<Map<String,String>> dataTable) {
+
+    }
+
 }
