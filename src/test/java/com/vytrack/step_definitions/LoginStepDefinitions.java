db65957f3d0a2c96fb984a5d12adda1181eab926
Like Sha <tradersalike88@yahoo.com>
2021-06-21 17:14:09 -0700

    Cucumber class LoginStepDefinitions class note update

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -43,6 +43,7 @@ public class LoginStepDefinitions {
         BrowserUtilities.waitForPageToLoad(10);
         BrowserUtilities.wait(2);
         Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+        Driver.closeDriver();
 
     }
 
