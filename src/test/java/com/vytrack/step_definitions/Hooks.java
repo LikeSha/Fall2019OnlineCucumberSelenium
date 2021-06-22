d276d5473c6ab74d15cf70d4c33a6b57feeb2d1d
Like Sha <tradersalike88@yahoo.com>
2021-06-22 13:32:36 -0700

    Hooks class note update

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -16,9 +16,13 @@ public class Hooks {
         Driver.getDriver().manage().window().maximize();
     }
 
-    @Before("@driver")
-    public void specialSetup(){
-        System.out.println("Setup for driver only");
+    @Before("@driver")// this special tags before specific scenario is very useful, when is useful?
+    public void specialSetup(){//when you have scenarios for database ,UI,API doing integration testing,
+        System.out.println("Setup for driver only");//we are testing how data is coming from another
+        //application,and we have to connect to another database besides our database. for this, we need
+        //to set another hook,that was responsible for connection for another database.in this case,
+        //the set up can configure only for specific test scenario. not for all scenarios, that specific
+        //scenario require to some specific setup.
     }
 
     @After("@driver")
