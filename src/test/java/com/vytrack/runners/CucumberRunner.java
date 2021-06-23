ed65e8661401a53d10db33c22b50896cd96f301e
Like Sha <tradersalike88@yahoo.com>
2021-06-22 19:03:42 -0700

    CucumberRunner class note update

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -779,5 +779,42 @@ make sure, that key name is correct.
         so the target folder cleaned up .no more ! then click "verify" to run the test,
         then go find out report under target folder --> cucumber html reports--> right click" overview-featrues.html
 
+about tags :
 
+if you put the tag on the top of Feature , means on the super top of the page, it means, this tag will
+run entire feature file, if you place the tag on a top of specific scenario, when you call the tag in
+runner class, it only run this specific scenario. example like below :
+
+@activities
+Feature: Create calendar event
+  As user, I want to be able to create calendar events.
+
+  Scenario: Create calendar event as sales manager
+    Given user is on the login page
+    When user logs in as a sales manager
+    And user navigates to "Activities" and "Calendar Events"
+    Then user clicks on create calendar event button
+    And user enters "On this meeting we discuss what went well, what went wrong and what can be improved" as a description
+    And user enters "Sprint Retrospective" as a title
+    Then user click on save and close button
+    And user verifies that description of new calendar event is "On this meeting we discuss what went well,
+    what went wrong and what can be improved"
+    And user verifies that title of new calendar event is "Sprint Retrospective"
+
+
+  @create_calendar_event_2
+  Scenario: Create calendar event as sales manager with data table
+    Given user is on the login page
+    When user logs in as a sales manager
+    And user navigates to "Activities" and "Calendar Events"
+    Then user clicks on create calendar event button
+    And user enters new calendar event information:
+      | description | On this meeting we discuss what went well, what went wrong and what can be improved |
+      | title       | Sprint Retrospective                                                                |
+    Then user click on save and close button
+    And user verifies new calendar event was created successfully
+      | description | On this meeting we discuss what went well, what went wrong and what can be improved |
+      | title       | Sprint Retrospective                                                                |
+
+the above entire Feature file will run if you call "activities"  tag in runner class
  */
